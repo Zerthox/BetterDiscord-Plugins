d747b83c4efe93dcc7a5d51ba918d65df56e486a
Zerthox <zerthox@gmail.com>
2019-04-04 16:30:22 +0200

    Fix wrong function usage

--- v1/BetterReplyer.plugin.js
+++ v1/BetterReplyer.plugin.js
@@ -1,312 +1,312 @@
-//META {"name": "BetterReplyer", "source": "https://github.com/Zerthox/BetterDiscord-Plugins/blob/master/plugins/BetterReplyer.plugin.js"} *//
+//META {"name": "BetterReplyer", "source": "https://github.com/Zerthox/BetterDiscord-Plugins/blob/master/v1/BetterReplyer.plugin.js"} *//
 
 /**
  * BetterReplyer plugin class
  * @author Zerthox
- * @version 3.1.0
+ * @version 3.1.1
  */
 class BetterReplyer {
 
-	/**
-	 * @return {string} plugin name
-	 */
-	getName() {
-		return "BetterReplyer";
-    }
-    
-    /**
-     * @return {*} plugin description
-     */
-	getDescription() {
-		return BdApi.React.createElement("span", {style: {"white-space": "pre-line"}},
-			"Reply to people using their ID with a button.\n Inspired by ",
-			BdApi.React.createElement("a", {href: "https://github.com/cosmicsalad/Discord-Themes-and-Plugins/blob/master/plugins/replyer.plugin.js", target: "_blank"}, "Replyer"),
-			" by ",
-			BdApi.React.createElement("a", {href: "https://github.com/cosmicsalad", target: "_blank"}, "@Hammock#3110"),
-			", ",
-			BdApi.React.createElement("a", {href: "https://github.com/Delivator", target: "_blank"}, "@Natsulus#0001"),
-			" & ",
-			BdApi.React.createElement("a", {href: "https://github.com/rauenzi", target: "_blank"}, "@Zerebos#7790"),
-			"."
-		);
-    }
-    
-    /**
-     * @return {string} plugin version
-     */
-	getVersion() {
-		return "3.1.0";
-	}
-    /**
-     * @return {*} plugin author
-     */
-	getAuthor() {
-		return BdApi.React.createElement("a", {href: "https://github.com/Zerthox", target: "_blank"}, "Zerthox");
-	}
+/**
+ * @return {string} plugin name
+ */
+getName() {
+	return "BetterReplyer";
+}
+
+/**
+ * @return {*} plugin description
+ */
+getDescription() {
+	return BdApi.React.createElement("span", {style: {"white-space": "pre-line"}},
+		"Reply to people using their ID with a button.\n Inspired by ",
+		BdApi.React.createElement("a", {href: "https://github.com/cosmicsalad/Discord-Themes-and-Plugins/blob/master/plugins/replyer.plugin.js", target: "_blank"}, "Replyer"),
+		" by ",
+		BdApi.React.createElement("a", {href: "https://github.com/cosmicsalad", target: "_blank"}, "@Hammock#3110"),
+		", ",
+		BdApi.React.createElement("a", {href: "https://github.com/Delivator", target: "_blank"}, "@Natsulus#0001"),
+		" & ",
+		BdApi.React.createElement("a", {href: "https://github.com/rauenzi", target: "_blank"}, "@Zerebos#7790"),
+		"."
+	);
+}
+
+/**
+ * @return {string} plugin version
+ */
+getVersion() {
+	return "3.1.1";
+}
+/**
+ * @return {*} plugin author
+ */
+getAuthor() {
+	return BdApi.React.createElement("a", {href: "https://github.com/Zerthox", target: "_blank"}, "Zerthox");
+}
+
+/**
+ * plugin class constructor
+ */
+constructor() {
 
 	/**
-	 * plugin class constructor
+	 * object with selectors
 	 */
-	constructor() {
+	this.selector = {
+		messageHeader: `.${BdApi.findModuleByProps("messagesWrapper", "messages").messages.split(" ")[0]} .${BdApi.findModuleByProps("headerCozyMeta").headerCozyMeta.split(" ")[0]}`,
+		channelTextarea: `.${BdApi.findModuleByProps("channelTextArea", "textArea").textArea.split(" ")[0]}`,
+		channelTextareaDisabled: `.${BdApi.findModuleByProps("channelTextArea", "textAreaDisabled").textAreaDisabled.split(" ")[0]}`,
+		messageContainer: `.${BdApi.findModuleByProps("message", "containerCozy").containerCozy.split(" ")[0]}`
+	};
+
+	/**
+	 * plugin styles
+	 */
+	this.css = `/* BetterReplyer CSS */
+		.replyer {
+			position: relative;
+			top: -1px;
+			margin-left: 5px;
+			padding: 3px 5px;
+			background: rgba(0, 0, 0, 0.4);
+			border-radius: 3px;
+			color: #fff !important;
+			font-size: 10px;
+			text-transform: uppercase;
+			cursor: pointer;
+		}
+		${this.selector.messageContainer}:not(:hover) .replyer {
+			visibility: hidden;
+		}`;
+
+	/**
+	 * object with plugin state relevant stuff
+	 */
+	this.state = {
+		/**
+		 * clicked element save
+		 */
+		clicked: document.body,
 
 		/**
-		 * object with selectors
+		 * focused textarea save
 		 */
-		this.selector = {
-			messageHeader: `.${BdApi.findModuleByProps("messagesWrapper", "messages").messages.split(" ")[0]} .${BdApi.findModuleByProps("headerCozyMeta").headerCozyMeta.split(" ")[0]}`,
-			channelTextarea: `.${BdApi.findModuleByProps("channelTextArea", "textArea").textArea.split(" ")[0]}`,
-			channelTextareaDisabled: `.${BdApi.findModuleByProps("channelTextArea", "textAreaDisabled").textAreaDisabled.split(" ")[0]}`,
-			messageContainer: `.${BdApi.findModuleByProps("message", "containerCozy").containerCozy.split(" ")[0]}`
-		};
+		focused: null,
 
 		/**
-		 * plugin styles
+		 * reply mode
 		 */
-		this.css = `/* BetterReplyer CSS */
-			.replyer {
-				position: relative;
-				top: -1px;
-				margin-left: 5px;
-				padding: 3px 5px;
-				background: rgba(0, 0, 0, 0.4);
-				border-radius: 3px;
-				color: #fff !important;
-				font-size: 10px;
-				text-transform: uppercase;
-				cursor: pointer;
-			}
-			${this.selector.messageContainer}:not(:hover) .replyer {
-				visibility: hidden;
-			}`;
-
-		/**
-		 * object with plugin state relevant stuff
-		 */
-		this.state = {
-			/**
-			 * clicked element save
-			 */
-			clicked: document.body,
-
-			/**
-			 * focused textarea save
-			 */
-			focused: null,
-
-			/**
-			 * reply mode
-			 */
-			mode: false
-		}
-
-		/**
-		 * object with handlers
-		 */
-		this.handler = {
-
-			/**
-			 * document click handler
-			 */
-			click: (e) => {
-
-				// save event target
-				this.state.clicked = e.target;
-			},
-
-			/**
-			 * textarea blur handler
-			 */
-			blur: (e) => {
-
-				// save event target
-				this.state.focused = e.target;
-
-				// update reply mode
-				this.state.mode = this.state.clicked.matches(`${this.selector.messageHeader} .replyer`) ? true : false;
-			}
-		};
-	}
-
-	 /**
-     * plugin start function
-     */
-	start() {
-
-		// inject styles
-		BdApi.injectCSS(this.getName(), this.css);
-
-		// add click handler
-		document.addEventListener("mousedown", this.handler.click);
-
-        // iterate over all message headers
-        for (var e of document.querySelectorAll(this.selector.messageHeader)) {
-
-            // insert reply button
-            this.insert(e);
-		}
-
-		// iterate over all textareas
-		for (var e of document.querySelectorAll(this.selector.channelTextarea)) {
-
-			// add blur handler
-			e.addEventListener("blur", this.handler.blur);
-		}
-
-        // console output
-		console.log(`[${this.getName()}] Enabled`);
-    }
-    
-    /**
-     * plugin stop function
-     */
-	stop() {
-
-		// remove styles
-		BdApi.clearCSS(this.getName());
-
-		// remove click handler
-		document.removeEventListener("mousedown", this.handler.click);
-
-        // iterate over all message headers
-        for (var e of document.querySelectorAll(`${this.selector.messageHeader} .replyer`)) {
-
-            // remove reply button
-            e.remove();
-		}
-
-		// iterate over all textareas
-		for (var e of document.querySelectorAll(this.selector.channelTextarea)) {
-
-			// remove blur handler
-			e.removeEventListener("blur", this.handler.blur);
-		}
-
-		// reset state
-		this.state = {
-			clicked: document.body,
-			focused: null,
-			mode: false
-		}
-		
-        // console output
-		console.log(`[${this.getName()}] Disabled`);
+		mode: false
 	}
 
 	/**
-	 * plugin DOM observer
-	 * @param {*} e event
+	 * object with handlers
 	 */
-	observer(e) {
+	this.handler = {
 
-		// check if channel textarea is disabled
-		if (document.querySelector(this.selector.channelTextareaDisabled) === null) {
-			
-			// iterate over added nodes
-			for (var n of e.addedNodes) {
-				
-				// check if node is html element
-				if (n instanceof HTMLElement) {
-					
-					// check if added nodes contain message header or textarea
-					if (n.matches(this.selector.messageHeader)) {
-						
-						// insert directly into message header
-						this.insert(n);
-					}
-					else if (n.matches(this.selector.channelTextarea)) {
-						
-						// add blur handler directly to textarea
-						n.addEventListener("blur", this.handler.blur);
-					}
-					else {
-						
-						// search for descendant message headers
-						var l = n.findAll(this.selector.messageHeader);
-						if (l.length > 0) {
-							
-							// insert into descendant message headers
-							for (var m of l) {
-								this.insert(m);
-							}
-						}
-						
-						// search for descendant textareas
-						l = n.findAll(this.selector.channelTextarea);
-						if (l.length > 0) {
-							
-							// add blur handler to descendant textareas
-							for (var t of l) {
-								t.addEventListener("blur", this.handler.blur);
-							}
-						}
-					}
-				}
-			}
+		/**
+		 * document click handler
+		 */
+		click: (e) => {
+
+			// save event target
+			this.state.clicked = e.target;
+		},
+
+		/**
+		 * textarea blur handler
+		 */
+		blur: (e) => {
+
+			// save event target
+			this.state.focused = e.target;
+
+			// update reply mode
+			this.state.mode = this.state.clicked.matches(`${this.selector.messageHeader} .replyer`) ? true : false;
 		}
+	};
+}
+
+ /**
+ * plugin start function
+ */
+start() {
+
+	// inject styles
+	BdApi.injectCSS(this.getName(), this.css);
+
+	// add click handler
+	document.addEventListener("mousedown", this.handler.click);
+
+	// iterate over all message headers
+	for (var e of document.querySelectorAll(this.selector.messageHeader)) {
+
+		// insert reply button
+		this.insert(e);
 	}
 
-	/**
-	 * insert reply button into passed message header
-	 * @param {HTMLElement} e message header
-	 */
-	insert(e) {
+	// iterate over all textareas
+	for (var e of document.querySelectorAll(this.selector.channelTextarea)) {
 
-		// get message author id
-		var id = BdApi.getInternalInstance(e).child.memoizedProps.message.author.id;
+		// add blur handler
+		e.addEventListener("blur", this.handler.blur);
+	}
 
-		// check if message is sent by user
-		if (id != BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id) {
+	// console output
+	console.log(`[${this.getName()}] Enabled`);
+}
+
+/**
+ * plugin stop function
+ */
+stop() {
+
+	// remove styles
+	BdApi.clearCSS(this.getName());
+
+	// remove click handler
+	document.removeEventListener("mousedown", this.handler.click);
+
+	// iterate over all message headers
+	for (var e of document.querySelectorAll(`${this.selector.messageHeader} .replyer`)) {
+
+		// remove reply button
+		e.remove();
+	}
+
+	// iterate over all textareas
+	for (var e of document.querySelectorAll(this.selector.channelTextarea)) {
+
+		// remove blur handler
+		e.removeEventListener("blur", this.handler.blur);
+	}
+
+	// reset state
+	this.state = {
+		clicked: document.body,
+		focused: null,
+		mode: false
+	}
 	
-			// create reply button element
-			var b = document.createElement("span");
-			
-			// set reply button class
-			b.className = "replyer";
-			
-			// set reply button text
-			b.innerHTML = "Reply";
-			
-			// add event listener
-			b.addEventListener("click", () => {
+	// console output
+	console.log(`[${this.getName()}] Disabled`);
+}
 
-				// get last focused textarea or default to first textarea found
-				var t = this.state.focused instanceof HTMLElement ? this.state.focused : document.querySelector(this.selector.channelTextarea);
+/**
+ * plugin DOM observer
+ * @param {*} e event
+ */
+observer(e) {
+
+	// check if channel textarea is disabled
+	if (document.querySelector(this.selector.channelTextareaDisabled) === null) {
+		
+		// iterate over added nodes
+		for (var n of e.addedNodes) {
+			
+			// check if node is html element
+			if (n instanceof HTMLElement) {
+				
+				// check if added nodes contain message header or textarea
+				if (n.matches(this.selector.messageHeader)) {
 					
-				// get mention string
-				var m = `<@!${id}>`;
-
-				// focus textarea
-				t.focus();
-
-				// check which mode to use
-				if (this.state.mode) {
-
-					// insert directly
-					document.execCommand("insertText", false, m);
+					// insert directly into message header
+					this.insert(n);
+				}
+				else if (n.matches(this.selector.channelTextarea)) {
+					
+					// add blur handler directly to textarea
+					n.addEventListener("blur", this.handler.blur);
 				}
 				else {
-
-					// add space to mention string
-					m += " ";
-
-					// save selection
-					var ts = t.selectionStart + m.length,
-						te = t.selectionEnd + m.length;
-				
-					// go to start of textarea
-					t.selectionEnd = 0;
-				
-					// insert mention
-					document.execCommand("insertText", false, m);
-				
-					// select saved selection
-					t.setSelectionRange(ts, te);
+					
+					// search for descendant message headers
+					var l = n.querySelectorAll(this.selector.messageHeader);
+					if (l.length > 0) {
+						
+						// insert into descendant message headers
+						for (var m of l) {
+							this.insert(m);
+						}
+					}
+					
+					// search for descendant textareas
+					l = n.querySelectorAll(this.selector.channelTextarea);
+					if (l.length > 0) {
+						
+						// add blur handler to descendant textareas
+						for (var t of l) {
+							t.addEventListener("blur", this.handler.blur);
+						}
+					}
 				}
-			});
-			
-			// append reply button
-			e.appendChild(b);
+			}
 		}
 	}
+}
+
+/**
+ * insert reply button into passed message header
+ * @param {HTMLElement} e message header
+ */
+insert(e) {
+
+	// get message author id
+	var id = BdApi.getInternalInstance(e).child.memoizedProps.message.author.id;
+
+	// check if message is sent by user
+	if (id != BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id) {
+
+		// create reply button element
+		var b = document.createElement("span");
+		
+		// set reply button class
+		b.className = "replyer";
+		
+		// set reply button text
+		b.innerHTML = "Reply";
+		
+		// add event listener
+		b.addEventListener("click", () => {
+
+			// get last focused textarea or default to first textarea found
+			var t = this.state.focused instanceof HTMLElement ? this.state.focused : document.querySelector(this.selector.channelTextarea);
+				
+			// get mention string
+			var m = `<@!${id}>`;
+
+			// focus textarea
+			t.focus();
+
+			// check which mode to use
+			if (this.state.mode) {
+
+				// insert directly
+				document.execCommand("insertText", false, m);
+			}
+			else {
+
+				// add space to mention string
+				m += " ";
+
+				// save selection
+				var ts = t.selectionStart + m.length,
+					te = t.selectionEnd + m.length;
+			
+				// go to start of textarea
+				t.selectionEnd = 0;
+			
+				// insert mention
+				document.execCommand("insertText", false, m);
+			
+				// select saved selection
+				t.setSelectionRange(ts, te);
+			}
+		});
+		
+		// append reply button
+		e.appendChild(b);
+	}
+}
 
 }
--- v1/GuildSeparators.plugin.js
+++ v1/GuildSeparators.plugin.js
@@ -1,263 +1,263 @@
-//META {"name": "GuildSeparators", "source": "https://github.com/Zerthox/BetterDiscord-Plugins/blob/master/plugins/GuildSeparators.plugin.js"} *//
+//META {"name": "GuildSeparators", "source": "https://github.com/Zerthox/BetterDiscord-Plugins/blob/master/v1/GuildSeparators.plugin.js"} *//
 
 /**
  * Guild Separators plugin class
  * @author Zerthox
- * @version 2.1.0
+ * @version 2.1.1
  */
 class GuildSeparators {
 
+/**
+ * @return {string} plugin name
+ */
+getName() {
+	return "GuildSeparators";
+}
+
+/**
+ * @return {*} plugin description
+ */
+getDescription() {
+	return BdApi.React.createElement("span", {style: {"white-space": "pre-line"}},
+		"Add Guild Separators with a button in the context menu."
+	);
+}
+
+/**
+ * @return {string} plugin version
+ */
+getVersion() {
+	return "2.1.1";
+}
+
+/**
+ * @return {*} plugin author
+ */
+getAuthor() {
+	return BdApi.React.createElement("a", {href: "https://github.com/Zerthox", target: "_blank"}, "Zerthox");
+}
+
+/**
+ * plugin class constructor
+ */
+constructor() {
+
 	/**
-	 * @return {string} plugin name
+	 * object with modules
 	 */
-	getName() {
-		return "GuildSeparators";
+	this.module = {
+		contextMenu: BdApi.findModuleByProps("getContextMenu")
 	}
 
-	/**
-	 * @return {*} plugin description
-	 */
-	getDescription() {
-		return BdApi.React.createElement("span", {style: {"white-space": "pre-line"}},
-			"Add Guild Separators with a button in the context menu."
-		);
-	}
+	// find context menu class module
+	var cm = BdApi.findModuleByProps("contextMenu", "item", "label");
 
 	/**
-	 * @return {string} plugin version
+	 * object with selectors
 	 */
-	getVersion() {
-		return "2.1.0";
-	}
+	this.selector = {
+		guild: `.${BdApi.findModuleByProps("container", "guildIcon").container.split(" ")[0]}`,
+		contextMenu: `.${cm.contextMenu.split(" ")[0]}`,
+		contextMenuItem: `.${cm.item.split(" ")[0]}`,
+		contextMenuLabel: `.${cm.label.split(" ")[0]}`
+	};
 
 	/**
-	 * @return {*} plugin author
+	 * plugin styles
 	 */
-	getAuthor() {
-		return BdApi.React.createElement("a", {href: "https://github.com/Zerthox", target: "_blank"}, "Zerthox");
-	}
-
-	/**
-	 * plugin class constructor
-	 */
-	constructor() {
-
-		/**
-		 * object with modules
-		 */
-		this.module = {
-			contextMenu: BdApi.findModuleByProps("getContextMenu")
+	this.css= `/* Guild Separators CSS */
+		${this.selector.guild}[separator] {
+			margin-bottom: 32px
 		}
+		${this.selector.guild}[separator]:after {
+			content: "";
+			position: absolute;
+			bottom: -16px;
+			left: 20%;
+			right: 20%;
+			height: 2px;
+			background: #2f3136;
+		}`;
+}
 
-		// find context menu class module
-		var cm = BdApi.findModuleByProps("contextMenu", "item", "label");
+/**
+ * plugin start function
+ */
+start() {
 
-		/**
-		 * object with selectors
-		 */
-		this.selector = {
-			guild: `.${BdApi.findModuleByProps("container", "guildIcon").container.split(" ")[0]}`,
-			contextMenu: `.${cm.contextMenu.split(" ")[0]}`,
-			contextMenuItem: `.${cm.item.split(" ")[0]}`,
-			contextMenuLabel: `.${cm.label.split(" ")[0]}`
+	// inject styles
+	BdApi.injectCSS(this.getName(), this.css);
+
+	// load guilds array from storage
+	this.guilds = BdApi.loadData(this.getName(), "guilds");
+
+	// check if loaded guilds array is empty
+	if (typeof this.guilds === "undefined") {
+
+		// reset guilds array
+		this.guilds = [];
+
+		// save empty guilds array
+		BdApi.saveData(this.getName(), "guilds", this.guilds);
+	}
+
+	// process guilds
+	this.processGuilds();
+
+	// check if context menu is present
+	var m = document.querySelector(`${this.selector.contextMenu}`);
+	if (m != null) {
+
+		// insert into context menu
+		this.insertContextMenu(m);
+	}
+
+	// console output
+	console.log(`[${this.getName()}] Enabled`);
+}
+
+/**
+ * plugin stop function
+ */
+stop() {
+
+	// remove styles
+	BdApi.clearCSS("GuildSeparators");
+
+	// save guilds array
+	BdApi.saveData(this.getName(), "guilds", this.guilds);
+
+	// remove separators
+	for (var e of document.querySelectorAll(`${this.selector.guild}[separator]`)) {
+		e.removeAttribute("separator");
+	}
+
+	// console output
+	console.log(`[${this.getName()}] Disabled`);
+}
+
+/**
+ * plugin DOM observer
+ * @param {*} e event
+ */
+observer(e) {
+
+	// iterate over added nodes
+	for (var n of e.addedNodes) {
+
+		// check if node is html element
+		if (n instanceof HTMLElement) {
+
+			// check if contextMenu or guild were added
+			if (n.matches(this.selector.contextMenu)) {
+
+				// insert context menu
+				this.insertContextMenu(n);
+			}
+			else if (n.matches(this.selector.guild) || n.querySelector(this.selector.guild) != null) {
+
+				// process guilds
+				this.processGuilds();
+			}
+		}
+	}
+}
+
+/**
+ * insert guild separator option into passed context menu
+ * @param {HTMLElement} m context menu element
+ */
+insertContextMenu(m) {
+
+	// get menu target
+	var t = this.module.contextMenu.getContextMenu().target;
+
+	// check if target is guild
+	var g = t.parents(this.selector.guild);
+	if (g.length > 0) {
+
+		// get guild element
+		g = g[0];
+
+		// get guild id
+		var id = this.getGuildId(g);
+
+		// clone context menu item
+		var i = m.querySelector("input[type=checkbox]").parents(this.selector.contextMenuItem)[0].cloneNode(true);
+
+		// set context menu item text
+		i.querySelector(this.selector.contextMenuLabel).innerHTML = "Add Separator";
+
+		// find context menu item checkbox
+		var c = i.querySelector("input[type=checkbox]");
+
+		// set checkbox checked property
+		c.checked = g.hasAttribute("separator");
+
+		// set checkbox onclick
+		i.onclick = () => {
+
+			// check if guild has separator
+			if (g.hasAttribute("separator")) {
+
+				// remove separator attribute
+				g.removeAttribute("separator");
+
+				// uncheck checkbox
+				c.checked = false;
+
+				// remove guild id from guilds array
+				this.guilds.splice(this.guilds.indexOf(id), 1);
+			}
+			else {
+
+				// remove separator attribute
+				g.setAttribute("separator", "");
+
+				// check checkbox
+				c.checked = true;
+
+				// add guild id to guilds array
+				this.guilds.push(id);
+			}
+
+			// save guilds array
+			BdApi.saveData(this.getName(), "guilds", this.guilds);
 		};
 
-		/**
-		 * plugin styles
-		 */
-		this.css= `/* Guild Separators CSS */
-			${this.selector.guild}[separator] {
-				margin-bottom: 32px
-			}
-			${this.selector.guild}[separator]:after {
-				content: "";
-				position: absolute;
-				bottom: -16px;
-				left: 20%;
-				right: 20%;
-				height: 2px;
-				background: #2f3136;
-			}`;
+		// append context menu item
+		m.appendChild(i);
+
+		// update context menu position
+		BdApi.getInternalInstance(m).return.memoizedProps.onHeightUpdate();
 	}
+}
 
-	/**
-	 * plugin start function
-	 */
-	start() {
+/**
+ * process guilds
+ */
+processGuilds() {
 
-		// inject styles
-		BdApi.injectCSS(this.getName(), this.css);
+	// iterate over guilds
+	for (var g of document.querySelectorAll(this.selector.guild)) {
 
-		// load guilds array from storage
-		this.guilds = BdApi.loadData(this.getName(), "guilds");
+		// check if guilds array contains guild
+		if (this.guilds.includes(this.getGuildId(g))) {
 
-		// check if loaded guilds array is empty
-		if (typeof this.guilds === "undefined") {
-
-			// reset guilds array
-			this.guilds = [];
-
-			// save empty guilds array
-			BdApi.saveData(this.getName(), "guilds", this.guilds);
-		}
-
-		// process guilds
-		this.processGuilds();
-
-		// check if context menu is present
-		var m = document.querySelector(`${this.selector.contextMenu}`);
-		if (m != null) {
-
-			// insert into context menu
-			this.insertContextMenu(m);
-		}
-
-		// console output
-		console.log(`[${this.getName()}] Enabled`);
-	}
-
-	/**
-	 * plugin stop function
-	 */
-	stop() {
-
-		// remove styles
-		BdApi.clearCSS("GuildSeparators");
-
-		// save guilds array
-		BdApi.saveData(this.getName(), "guilds", this.guilds);
-
-		// remove separators
-		for (var e of document.querySelectorAll(`${this.selector.guild}[separator]`)) {
-			e.removeAttribute("separator");
-		}
-
-		// console output
-		console.log(`[${this.getName()}] Disabled`);
-	}
-
-	/**
-	 * plugin DOM observer
-	 * @param {*} e event
-	 */
-	observer(e) {
-
-		// iterate over added nodes
-        for (var n of e.addedNodes) {
-
-            // check if node is html element
-            if (n instanceof HTMLElement) {
-
-                // check if contextMenu or guild were added
-                if (n.matches(this.selector.contextMenu)) {
-
-                    // insert context menu
-                    this.insertContextMenu(n);
-                }
-                else if (n.matches(this.selector.guild) || n.find(this.selector.guild) != null) {
-
-                    // process guilds
-                    this.processGuilds();
-                }
-            }
-        }
-	}
-
-	/**
-	 * insert guild separator option into passed context menu
-	 * @param {HTMLElement} m context menu element
-	 */
-	insertContextMenu(m) {
-
-		// get menu target
-		var t = this.module.contextMenu.getContextMenu().target;
-
-		// check if target is guild
-		var g = t.parents(this.selector.guild);
-		if (g.length > 0) {
-
-			// get guild element
-			g = g[0];
-
-			// get guild id
-			var id = this.getGuildId(g);
-
-			// clone context menu item
-			var i = m.find("input[type=checkbox]").parents(this.selector.contextMenuItem)[0].cloneNode(true);
-
-			// set context menu item text
-			i.find(this.selector.contextMenuLabel).innerHTML = "Add Separator";
-
-			// find context menu item checkbox
-			var c = i.find("input[type=checkbox]");
-
-			// set checkbox checked property
-			c.checked = g.hasAttribute("separator");
-
-			// set checkbox onclick
-			i.onclick = () => {
-
-				// check if guild has separator
-				if (g.hasAttribute("separator")) {
-
-					// remove separator attribute
-					g.removeAttribute("separator");
-
-					// uncheck checkbox
-					c.checked = false;
-
-					// remove guild id from guilds array
-					this.guilds.splice(this.guilds.indexOf(id), 1);
-				}
-				else {
-
-					// remove separator attribute
-					g.setAttribute("separator", "");
-
-					// check checkbox
-					c.checked = true;
-
-					// add guild id to guilds array
-					this.guilds.push(id);
-				}
-
-				// save guilds array
-				BdApi.saveData(this.getName(), "guilds", this.guilds);
-			};
-
-			// append context menu item
-			m.appendChild(i);
-
-			// update context menu position
-			BdApi.getInternalInstance(m).return.memoizedProps.onHeightUpdate();
+			// add separator attribute
+			g.setAttribute("separator", "");
 		}
 	}
+}
 
-	/**
-	 * process guilds
-	 */
-	processGuilds() {
-
-		// iterate over guilds
-		for (var g of document.querySelectorAll(this.selector.guild)) {
-
-			// check if guilds array contains guild
-			if (this.guilds.includes(this.getGuildId(g))) {
-
-				// add separator attribute
-				g.setAttribute("separator", "");
-			}
-		}
-	}
-
-	/**
-	 * get guild id of passed guild element
-	 * @param {HTMLElement} e guild element
-	 * @return {string} guild id
-	 */
-	getGuildId(e) {
-		var r = BdApi.getInternalInstance(e);
-		return r && r.return.memoizedProps.guild.id;
-	}
+/**
+ * get guild id of passed guild element
+ * @param {HTMLElement} e guild element
+ * @return {string} guild id
+ */
+getGuildId(e) {
+	var r = BdApi.getInternalInstance(e);
+	return r && r.return.memoizedProps.guild.id;
+}
 
 }
