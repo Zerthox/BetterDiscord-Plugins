084feb78b0ed220a168937bd9f8cf4527b5005dc
Zerthox <zerthox@gmail.com>
2021-05-05 19:24:15 +0200

    Add eslint & update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    env: {
+        node: true
+    },
+    plugins: [
+        "node",
+        "react"
+    ],
+    settings: {
+        react: {
+            version: "16.12.0" // stable Discord
+        }
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "google"
+    ],
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+        "comma-dangle": ["error", "never"],
+        "quote-props": ["error", "as-needed"],
+        "max-len": "off",
+        "no-undef": "off",
+        "require-jsdoc": "off",
+        "valid-jsdoc": "off",
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/display-name": "off"
+    }
+};
