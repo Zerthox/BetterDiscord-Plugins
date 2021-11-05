be92fdd30e8b527312af18a48169db75a3c06cb7
Zerthox <zerthox@gmail.com>
2021-11-05 17:57:42 +0100

    Implement watching

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,10 +2,13 @@ import path from "path";
 import {promises as fs, readdirSync, readFileSync} from "fs";
 import minimist from "minimist";
 import chalk from "chalk";
-import {rollup} from "rollup";
+import * as rollup from "rollup";
 import rollupConfig from "../rollup.config";
 import {Config} from "discordium";
 
+const repo = "https://github.com/Zerthox/BetterDiscord-Plugins";
+
+const success = (msg: string) => console.log(chalk.green(msg));
 const warn = (msg: string) => console.warn(chalk.yellow(`Warn: ${msg}`));
 const error = (msg: string) => console.error(chalk.red(`Error: ${msg}`));
 
@@ -16,7 +19,7 @@ const sourceEntries = (readdirSync(sourceFolder, {withFileTypes: true}))
 const wscript = readFileSync(path.resolve(__dirname, "wscript.js"), "utf8").split("\n").filter((line) => line.trim().length > 0).join("\n");
 
 // parse args
-const args = minimist(process.argv.slice(2), {boolean: "dev"});
+const args = minimist(process.argv.slice(2), {boolean: ["dev", "watch"]});
 
 // resolve input paths
 let inputs: string[] = [];
@@ -39,53 +42,115 @@ if (inputs.length === 0) {
     process.exit(1);
 }
 
-const output = args.dev ? path.resolve(
+// resolve output directory
+const outDir = args.dev ? path.resolve(
     process.platform === "win32" ? process.env.APPDATA
         : process.platform === "darwin" ? path.resolve(process.env.HOME, "Library/Preferences")
             : path.resolve(process.env.HOME, ".config"),
     "BetterDiscord/plugins"
 ) : path.resolve(__dirname, "../dist");
 
+const watchers: Record<string, rollup.RollupWatcher> = {};
+
 // build each input
 for (const input of inputs) {
-    build(input, output);
+    const output = path.resolve(outDir, `${path.basename(input)}.plugin.js`);
+
+    if (args.watch) {
+        // watch for changes
+        watch(input, output).then(() => console.log(`Watching for changes in "${input}"`));
+    } else {
+        // build once
+        build(input, output);
+    }
+}
+if (args.watch) {
+    // keep process alive
+    process.stdin.resume();
+    process.stdin.on("end", () => {
+        for (const watcher of Object.values(watchers)) {
+            watcher.close();
+        }
+    });
 }
 
 async function build(input: string, output: string) {
     // parse config
-    const config = await resolveConfig(input);
+    const config = await readConfig(input);
     const {output: outputConfig, ...inputConfig} = rollupConfig;
 
-    // resolve output file
-    const outFile = path.resolve(output, `${path.basename(input)}.plugin.js`);
-
     // bundle plugin
-    const bundle = await rollup({
+    const bundle = await rollup.rollup({
         ...inputConfig,
         input: path.resolve(input, "index.tsx")
     });
     await bundle.write({
         ...outputConfig,
-        file: outFile,
-        banner: toMeta(config) + `\n/*@cc_on @if (@_jscript)\n${wscript}\n@else @*/\n`,
-        footer: "\n/*@end @*/"
+        ...genOutputOptions(config, output)
     });
-    await bundle.close();
+    success(`Built ${config.name} v${config.version} to "${output}"`);
 
-    console.log(chalk.green(`Built ${config.name} v${config.version} to "${outFile}"`));
+    await bundle.close();
+}
+
+async function watch(input: string, output: string) {
+    const config = await readConfig(input);
+    const {output: outputConfig, ...inputConfig} = rollupConfig;
+
+    // start watching
+    const watcher = rollup.watch({
+        ...inputConfig,
+        input: path.resolve(input, "index.tsx"),
+        output: {
+            ...outputConfig,
+            ...genOutputOptions(config, output)
+        }
+    });
+
+    // close finished bundles
+    watcher.on("event", (event) => {
+        if (event.code === "BUNDLE_END") {
+            success(`Built ${config.name} v${config.version} to "${output}" [${event.duration}ms]`);
+            event.result.close();
+        }
+    });
+
+    // restart on config changes
+    const configPath = resolveConfig(input);
+    watcher.on("change", (file) => {
+        // check for config changes
+        if (file === configPath) {
+            watchers[input].close();
+            watch(input, output);
+        }
+
+        console.log(`=> Changed "${file}"`);
+    });
+
+    watchers[input] = watcher;
 }
 
 interface Meta extends Config {
-    authorLink?: URL;
-    updateUrl?: URL;
-    website?: URL;
-    source?: URL;
-    donate?: URL;
+    authorLink?: string;
+    updateUrl?: string;
+    website?: string;
+    source?: string;
+    donate?: string;
 }
 
-async function resolveConfig(input: string): Promise<Meta> {
-    const config = JSON.parse(await fs.readFile(path.resolve(input, "config.json"), "utf8")) as Config;
-    return {...config};
+function resolveConfig(input: string): string {
+    return path.resolve(input, "config.json");
+}
+
+async function readConfig(input: string): Promise<Meta> {
+    const config = JSON.parse(await fs.readFile(resolveConfig(input), "utf8")) as Config;
+    return {
+        ...config,
+        authorLink: `https://github.com/${config.author}`,
+        website: repo,
+        source: `${repo}/tree/master/src/${path.basename(input)}`,
+        updateUrl: `${repo}/blob/master/dist/${path.basename(input)}.plugin.js`
+    };
 }
 
 function toMeta(config: Config): string {
@@ -94,4 +159,12 @@ function toMeta(config: Config): string {
         result += `\n * @${key} ${value.replace(/\n/g, "\\n")}`;
     }
     return result + "\n**/\n";
+}
+
+function genOutputOptions(config: Config, output: string) {
+    return {
+        file: output,
+        banner: toMeta(config) + `\n/*@cc_on @if (@_jscript)\n${wscript}\n@else @*/\n`,
+        footer: "\n/*@end @*/"
+    };
 }
