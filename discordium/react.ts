c1e2df881fe8478d10791351743f9aa606beb76f
Zerthox <zerthox@gmail.com>
2021-11-10 20:03:43 +0100

    Fix typo

--- discordium/react.ts
+++ discordium/react.ts
@@ -13,7 +13,7 @@ export interface ReactInternals {
 export const ReactInternals: ReactInternals = (React as any)?.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
 export interface ReactDOMInternals {
-    getInstancefromNode(node: Node): Fiber;
+    getInstanceFromNode(node: Node): Fiber;
     getNodeFromInstance(inst: Fiber): Node;
     getFiberCurrentPropsFromNode(node: Node): Record<string, any>;
     enqueueStateRestore(target: Node): void;
