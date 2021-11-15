156218090996f64e94d000cd01da4b4ef2529f6a
Zerthox <zerthox@gmail.com>
2021-11-15 15:01:40 +0100

    Add import resolution

--- discordium/finder.ts
+++ discordium/finder.ts
@@ -1,7 +1,19 @@
-interface Require {
+export interface Module {
+    id: number;
+    loaded: boolean;
+    exports: Record<any, any>;
+}
+
+type ModuleFunction = (this: Module["exports"], module: Module, exports: Module["exports"], require: Require) => void;
+
+export interface Require {
     (id: number): any;
-    m: Record<string, any>;
-    c: Record<string, any>;
+
+    /** module register */
+    m: Record<string, ModuleFunction>;
+
+    /** module cache */
+    c: Record<string, Module>;
 }
 
 // grab webpack require then cleanup
@@ -21,7 +33,7 @@ delete webpackRequire.c.__discordium__;
 export type Filter = (exports: any, module?: any) => boolean;
 
 // TODO: option to check filter on props?
-const applyFilters = (filters: Filter[]) => {
+const joinFilters = (filters: Filter[]) => {
     return (module: {exports: any}) => {
         const {exports} = module;
         return filters.every((filter) => filter(exports, module) || (exports?.__esModule && filter(exports?.default, module)));
@@ -72,16 +84,16 @@ const genFilters = ({filter, name, props, protos, source}: FilterOptions): Filte
 const raw = {
     require: webpackRequire,
     getAll: () => Object.values(webpackRequire.c),
-    find: (...filters: Filter[]) => raw.getAll().find(applyFilters(filters)) ?? null,
+    find: (...filters: Filter[]) => raw.getAll().find(joinFilters(filters)) ?? null,
     query: (options: FilterOptions) => raw.find(...genFilters(options)),
-    byId: (id: number) => webpackRequire.c[id] ?? null,
+    byId: (id: number | string) => webpackRequire.c[id] ?? null,
     byExports: (exported: unknown) => raw.find(filters.byExports(exported)),
     byName: (name: string) => raw.find(filters.byName(name)),
     byProps: (...props: string[]) => raw.find(filters.byProps(props)),
     byProtos: (...protos: string[]) => raw.find(filters.byProtos(protos)),
     bySource: (...contents: string[]) => raw.find(filters.bySource(contents)),
     all: {
-        find: (...filters: Filter[]) => raw.getAll().filter(applyFilters(filters)),
+        find: (...filters: Filter[]) => raw.getAll().filter(joinFilters(filters)),
         query: (options: FilterOptions) => raw.all.find(...genFilters(options)),
         byExports: (exported: unknown) => raw.all.find(filters.byExports(exported)),
         byName: (name: string) => raw.all.find(filters.byName(name)),
@@ -89,7 +101,7 @@ const raw = {
         byProtos: (...protos: string[]) => raw.all.find(filters.byProtos(protos)),
         bySource: (...contents: string[]) => raw.all.find(filters.bySource(contents))
     },
-    resolveExports: (module: any, filter: string | ((entry: any) => boolean) | null = null) => {
+    resolveExports(module: Module, filter: string | ((entry: any) => boolean) | null = null) {
         if (module instanceof Object && "exports" in module) {
             const exported = module.exports;
             if (!exported) {
@@ -107,15 +119,36 @@ const raw = {
             }
 
             // check for default export
-            if (exported instanceof Object && exported.__esModule && "default" in exported && Object.keys(exported).length === 1) {
+            if (exported.__esModule && "default" in exported && Object.keys(exported).length === 1) {
                 return exported.default;
+            } else {
+                return exported;
             }
-
-            return exported;
         }
 
         return null;
-    }
+    },
+    resolveImports(module: Module) {
+        // get module as source code
+        const source = webpackRequire.m[module.id].toString();
+
+        // find require parameter name
+        const match = source.match(/^(?:function)?\s*\(\w+,\w+,(\w+)\)\s*(?:=>)?\s*{/);
+        if (match) {
+            // find require calls
+            const requireName = match[1];
+            const calls = Array.from(source.matchAll(new RegExp(`\\W${requireName}\\((\\d+)\\)`, "g")));
+            return calls.map((call) => raw.byId(call[1]));
+        } else {
+            return [];
+        }
+    },
+    resolveStyles: (module: Module) => raw.resolveImports(module).filter((imported) => (
+        imported instanceof Object
+        && "exports" in imported
+        && Object.values(imported.exports).every((value) => typeof value === "string")
+        && Object.entries(imported.exports).find(([key, value]: [string, string]) => (new RegExp(`^${key}-([a-zA-Z0-9-_]){6}(\\s.+)$`)).test(value))
+    ))
 };
 
 const Finder = {
@@ -123,12 +156,14 @@ const Finder = {
     getAll: () => raw.getAll().map((entry) => raw.resolveExports(entry)),
     find: (...filters: Filter[]) => raw.resolveExports(raw.find(...filters)),
     query: (options: Options) => raw.resolveExports(raw.query(options), options.export),
-    byId: (id: number) => raw.resolveExports(raw.byId(id)),
+    byId: (id: number | string) => raw.resolveExports(raw.byId(id)),
     byExports: (exported: unknown) => raw.resolveExports(raw.byExports(exported)),
     byName: (name: string) => raw.resolveExports(raw.byName(name), filters.byDisplayName(name)),
     byProps: (...props: string[]) => raw.resolveExports(raw.byProps(...props), filters.byProps(props)),
     byProtos: (...protos: string[]) => raw.resolveExports(raw.byProtos(...protos), filters.byProtos(protos)),
     bySource: (...contents: string[]) => raw.resolveExports(raw.bySource(...contents), filters.bySource(contents)),
+    resolveImports: (exported: any) => raw.resolveImports(raw.byExports(exported)).map((entry) => raw.resolveExports(entry)),
+    resolveStyles: (exported: any) => raw.resolveStyles(raw.byExports(exported)).map((entry) => raw.resolveExports(entry)),
     all: {
         find: (...filters: Filter[]) => raw.all.find(...filters).map((entry) => raw.resolveExports(entry)),
         query: (options: Options) => raw.all.query(options).map((entry) => raw.resolveExports(entry, options.export)),
