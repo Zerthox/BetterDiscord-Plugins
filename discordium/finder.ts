6a3e5ca31775b97f9abc3335e8fa33f3d131a7a9
Zerthox <zerthox@gmail.com>
2022-01-11 17:55:41 +0100

    Update finder interface

--- discordium/finder.ts
+++ discordium/finder.ts
@@ -18,47 +18,53 @@ export interface Require {
     c: Record<string, Module>;
 }
 
-// grab webpack require then cleanup
-let webpackRequire: Require;
-global.webpackJsonp.push([
-    [],
-    {
-        __discordium__: (_module: Module, _exports: Exports, require: Require) => {
+const getWebpackRequire = (): Require => {
+    const moduleId = "discordium";
+
+    let webpackRequire: Require;
+    global.webpackJsonp.push([[], {
+        [moduleId]: (_module: Module, _exports: Exports, require: Require) => {
             webpackRequire = require;
         }
-    },
-    [["__discordium__"]]
-]);
-delete webpackRequire.m.__discordium__;
-delete webpackRequire.c.__discordium__;
+    }, [[moduleId]]]);
 
-export type Filter = (exports: Exports, module?: Module) => boolean;
+    // cleanup
+    delete webpackRequire.m[moduleId];
+    delete webpackRequire.c[moduleId];
+
+    return webpackRequire;
+};
+
+export type Filter = (exports: Exports, module: Module) => boolean;
 
 // TODO: option to check filter on props?
 const joinFilters = (filters: Filter[]) => {
     return (module: Module) => {
         const {exports} = module;
-        return filters.every((filter) => filter(exports, module) || (exports?.__esModule && filter(exports?.default, module)));
+        return (
+            filters.every((filter) => filter(exports, module))
+            || exports?.__esModule && "default" in exports && filters.every((filter) => filter(exports.default, module))
+        );
     };
 };
 
-const filters = {
-    byExports(exported: Exports): Filter {
+export const filters = {
+    byExports(exported: Exports) {
         return (target) => target === exported || (target instanceof Object && Object.values(target).includes(exported));
     },
-    byName(name: string): Filter {
+    byName(name: string) {
         return (target) => target instanceof Object && Object.values(target).some(filters.byDisplayName(name) as any);
     },
-    byDisplayName(name: string): Filter {
+    byDisplayName(name: string) {
         return (target: any) => target?.displayName === name || target?.constructor?.displayName === name;
     },
-    byProps(props: string[]): Filter {
+    byProps(props: string[]) {
         return (target) => target instanceof Object && props.every((prop) => prop in target);
     },
-    byProtos(protos: string[]): Filter {
+    byProtos(protos: string[]) {
         return (target: any) => target instanceof Object && target.prototype instanceof Object && protos.every((proto) => proto in target.prototype);
     },
-    bySource(contents: string[]): Filter {
+    bySource(contents: string[]) {
         // TODO: allow regex?
         return (target) => target instanceof Function && contents.every((content) => target.toString().includes(content));
     }
@@ -76,6 +82,12 @@ export interface Options extends FilterOptions {
     export?: string;
 }
 
+export interface ResolveOptions {
+    filter?: (target: any) => boolean;
+    export?: string;
+    name?: string;
+}
+
 const genFilters = ({filter, name, props, protos, source}: FilterOptions): Filter[] => [
     ...[filter].flat(),
     typeof name === "string" ? filters.byName(name) : null,
@@ -85,13 +97,13 @@ const genFilters = ({filter, name, props, protos, source}: FilterOptions): Filte
 ].filter((entry) => entry instanceof Function);
 
 export const raw = {
-    require: webpackRequire,
-    getAll: () => Object.values(webpackRequire.c),
-    getSources: () => Object.values(webpackRequire.m),
-    getSource: (id: number | string) => webpackRequire.m[id] ?? null,
+    require: getWebpackRequire(),
+    getAll: () => Object.values(raw.require.c),
+    getSources: () => Object.values(raw.require.m),
+    getSource: (id: number | string) => raw.require.m[id] ?? null,
     find: (...filters: Filter[]) => raw.getAll().find(joinFilters(filters)) ?? null,
     query: (options: FilterOptions) => raw.find(...genFilters(options)),
-    byId: (id: number | string) => webpackRequire.c[id] ?? null,
+    byId: (id: number | string) => raw.require.c[id] ?? null,
     byExports: (exported: Exports) => raw.find(filters.byExports(exported)),
     byName: (name: string) => raw.find(filters.byName(name)),
     byProps: (...props: string[]) => raw.find(filters.byProps(props)),
@@ -106,25 +118,26 @@ export const raw = {
         byProtos: (...protos: string[]) => raw.all.find(filters.byProtos(protos)),
         bySource: (...contents: string[]) => raw.all.find(filters.bySource(contents))
     },
-    resolveExports(module: Module, filter: string | ((entry: any) => boolean) | null = null) {
+    resolveExports(module: Module, options: ResolveOptions = {}) {
         if (module instanceof Object && "exports" in module) {
             const exported = module.exports;
             if (!exported) {
                 return exported;
             }
 
-            // apply filter
-            if (typeof filter === "string") {
-                return exported[filter];
-            } else if (filter instanceof Function) {
-                const result = Object.values(exported).find((value) => filter(value));
-                if (result !== undefined) {
-                    return result;
-                }
+            const hasDefault = exported.__esModule && "default" in exported;
+
+            // apply options
+            if (options.export) {
+                return exported[options.export];
+            } else if (options.name) {
+                return Object.values(exported).find(filters.byDisplayName(options.name));
+            } else if (options.filter && hasDefault && options.filter(exported.default)) {
+                return exported.default;
             }
 
-            // check for default export
-            if (exported.__esModule && "default" in exported && Object.keys(exported).length === 1) {
+            // check for single default export
+            if (hasDefault && Object.keys(exported).length === 1) {
                 return exported.default;
             } else {
                 return exported;
@@ -165,7 +178,7 @@ export const getAll = () => raw.getAll().map((entry) => raw.resolveExports(entry
 export const find = (...filters: Filter[]) => raw.resolveExports(raw.find(...filters));
 
 /** Finds a module using query options. */
-export const query = (options: Options) => raw.resolveExports(raw.query(options), options.export);
+export const query = (options: Options) => raw.resolveExports(raw.query(options), {export: options.export});
 
 /**
  * Finds a module using its id.
@@ -178,16 +191,16 @@ export const byId = (id: number | string) => raw.resolveExports(raw.byId(id));
 export const byExports = (exported: Exports) => raw.resolveExports(raw.byExports(exported));
 
 /** Finds a module using the name of its export. */
-export const byName = (name: string) => raw.resolveExports(raw.byName(name), filters.byDisplayName(name));
+export const byName = (name: string) => raw.resolveExports(raw.byName(name), {name});
 
 /** Finds a module using property names of its export. */
-export const byProps = (...props: string[]) => raw.resolveExports(raw.byProps(...props), filters.byProps(props));
+export const byProps = (...props: string[]) => raw.resolveExports(raw.byProps(...props), {filter: filters.byProps(props)});
 
 /** Finds a module using prototype names of its export. */
-export const byProtos = (...protos: string[]) => raw.resolveExports(raw.byProtos(...protos), filters.byProtos(protos));
+export const byProtos = (...protos: string[]) => raw.resolveExports(raw.byProtos(...protos), {filter: filters.byProtos(protos)});
 
 /** Finds a module using source code contents of its export entries. */
-export const bySource = (...contents: string[]) => raw.resolveExports(raw.bySource(...contents), filters.bySource(contents));
+export const bySource = (...contents: string[]) => raw.resolveExports(raw.bySource(...contents), {filter: filters.bySource(contents)});
 
 /** Returns module ids of all other modules imported in the module. */
 export const resolveImportIds = (exported: Exports) => raw.resolveImportIds(raw.byExports(exported));
@@ -207,20 +220,20 @@ export const all = {
     find: (...filters: Filter[]) => raw.all.find(...filters).map((entry) => raw.resolveExports(entry)),
 
     /** Finds all modules using query options. */
-    query: (options: Options) => raw.all.query(options).map((entry) => raw.resolveExports(entry, options.export)),
+    query: (options: Options) => raw.all.query(options).map((entry) => raw.resolveExports(entry, {export: options.export})),
 
     /** Finds all modules using the exports. */
     byExports: (exported: Exports) => raw.all.byExports(exported).map((entry) => raw.resolveExports(entry)),
 
     /** Finds all modules using the name of its export. */
-    byName: (name: string) => raw.all.byName(name).map((entry) => raw.resolveExports(entry, filters.byDisplayName(name))),
+    byName: (name: string) => raw.all.byName(name).map((entry) => raw.resolveExports(entry, {name})),
 
     /** Finds all modules using property names of its export. */
-    byProps: (...props: string[]) => raw.all.byProps(...props).map((entry) => raw.resolveExports(entry, filters.byProps(props))),
+    byProps: (...props: string[]) => raw.all.byProps(...props).map((entry) => raw.resolveExports(entry, {filter: filters.byProps(props)})),
 
     /** Finds all modules using prototype names of it export. */
-    byProtos: (...protos: string[]) => raw.all.byProtos(...protos).map((entry) => raw.resolveExports(entry, filters.byProtos(protos))),
+    byProtos: (...protos: string[]) => raw.all.byProtos(...protos).map((entry) => raw.resolveExports(entry, {filter: filters.byProtos(protos)})),
 
     /** Finds all modules using source code contents of its export entries. */
-    bySource: (...contents: string[]) => raw.all.bySource(...contents).map((entry) => raw.resolveExports(entry, filters.bySource(contents)))
+    bySource: (...contents: string[]) => raw.all.bySource(...contents).map((entry) => raw.resolveExports(entry, {filter: filters.bySource(contents)}))
 };
