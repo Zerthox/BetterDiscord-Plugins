77c72065548e809aa861cff24621bc30f3529641
Zerthox <zerthox@gmail.com>
2022-07-25 18:27:23 +0200

    Remove cache

--- dium/src/modules/index.ts
+++ dium/src/modules/index.ts
@@ -11,25 +11,3 @@ export type UntypedStore = Untyped<Store>;
 export type UntypedComponent = Untyped<React.ComponentType<any>>;
 
 export type StyleModule = Record<string, string>;
-
-type ModuleCache<T extends Record<string, () => any>> = {
-    [P in keyof T]: ReturnType<T[P]>;
-};
-
-export const createCache = <T extends Record<string, () => any>>(
-    entries: T
-): ModuleCache<T> => {
-    const result = {};
-    for (const [key, value] of Object.entries(entries)) {
-        Object.defineProperty(result, key, {
-            enumerable: true,
-            configurable: true,
-            get() {
-                delete this[key];
-                this[key] = value();
-                return this[key];
-            }
-        });
-    }
-    return result as ModuleCache<T>;
-};
