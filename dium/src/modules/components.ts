fa786418470f7dc58c52781f99c1da2b2fae74a8
Zerthox <zerthox@gmail.com>
2022-08-27 14:23:53 +0200

    Add margins typing

--- dium/src/modules/components.ts
+++ dium/src/modules/components.ts
@@ -4,7 +4,6 @@ import {Untyped} from ".";
 // TODO: split components from modules?
 
 type UntypedComponent = Untyped<React.ComponentType<any>>;
-type StyleModule = Record<string, string>;
 
 export const Flex: UntypedComponent = /* @__PURE__ */ Finder.byAnyName("Flex");
 export const Button: UntypedComponent = /* @__PURE__ */ Finder.byProps("Link", "Hovers");
@@ -20,4 +19,24 @@ export const TextInput: UntypedComponent = /* @__PURE__ */ Finder.byAnyName("Tex
 export const Menu: Record<string, UntypedComponent> = /* @__PURE__ */ Finder.byProps("MenuGroup", "MenuItem", "MenuSeparator");
 export const Form: Record<string, UntypedComponent> = /* @__PURE__ */ Finder.byProps("FormItem", "FormSection", "FormDivider");
 
-export const margins: StyleModule = /* @__PURE__ */ Finder.byProps("marginLarge");
+interface Margins {
+    marginBottom4: string;
+    marginBottom8: string;
+    marginBottom20: string;
+    marginBottom40: string;
+    marginBottom60: string;
+    marginCenterHorz: string;
+    marginLeft8: string;
+    marginReset: string;
+    marginTop4: string;
+    marginTop8: string;
+    marginTop20: string;
+    marginTop40: string;
+    marginTop60: string;
+    marginXLarge: "60px";
+    marginLarge: "40px";
+    marginMedium: "20px";
+    marginSmall: "8px";
+    marginXSmall: "4px";
+}
+export const margins: Margins = /* @__PURE__ */ Finder.byProps("marginLarge");
