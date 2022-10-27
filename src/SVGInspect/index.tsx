b63b6a5868a8294ba288fbfb978abefdeecd46e3
Zerthox <zerthox@gmail.com>
2022-10-27 20:23:27 +0200

    Add svg inspect plugin

---
+++ src/SVGInspect/index.tsx
@@ -0,0 +1,38 @@
+import {createPlugin, createSettings, Finder, React} from "dium";
+
+const components = Finder.all.bySource([".createElement(\"svg\","], {entries: true});
+
+interface ErrorBoundaryProps {
+    children?: React.ReactChild;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = {hasError: false};
+    }
+    render() {
+        return !this.state.hasError ? this.props.children : null;
+    }
+    static getDerivedStateFromError() {
+        return {hasError: true};
+    }
+}
+
+export default createPlugin({
+    start() {},
+    Settings: createSettings({}),
+    SettingsPanel: () => (
+        <>
+            {components.map((SVG, i) => (
+                <ErrorBoundary key={i}>
+                    <SVG open/>
+                </ErrorBoundary>
+            ))}
+        </>
+    )
+});
