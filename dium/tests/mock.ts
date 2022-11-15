a5925a1bd0fdf0e02173ecce0653bd08b33d50b6
Zerthox <zerthox@gmail.com>
2022-11-15 16:53:39 +0100

    Add tests for query fiber

--- dium/tests/mock.ts
+++ dium/tests/mock.ts
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactReconciler, {Fiber} from "react-reconciler";
+import {LegacyRoot} from "react-reconciler/constants";
+
 (global as any).TESTING = true;
 
 (global as any).BdApi = {
@@ -7,4 +11,46 @@
     Plugins: {
         get: () => ({})
     }
-} as any;
+};
+
+const noop: any = () => {};
+
+const reconciler = ReactReconciler({
+    supportsMutation: true,
+    isPrimaryRenderer: true,
+    supportsHydration: false,
+    createContainerChildSet: noop,
+    appendChildToContainerChildSet: noop,
+    finalizeContainerChildren: noop,
+    clearContainer: noop,
+    appendChildToContainer: noop,
+    getRootHostContext: noop,
+    prepareForCommit: noop,
+    resetAfterCommit: noop,
+    getChildHostContext: noop,
+    shouldSetTextContent: noop,
+    createInstance: noop,
+    createTextInstance: noop,
+    appendInitialChild: noop,
+    finalizeInitialChildren: noop,
+    supportsPersistence: noop,
+    prepareUpdate: noop,
+    preparePortalMount: noop,
+    getPublicInstance: noop,
+    getInstanceFromNode: noop,
+    getCurrentEventPriority: noop,
+    beforeActiveInstanceBlur: noop,
+    afterActiveInstanceBlur: noop,
+    prepareScopeUpdate: noop,
+    getInstanceFromScope: noop,
+    detachDeletedInstance: noop,
+    scheduleTimeout: noop,
+    cancelTimeout: noop,
+    noTimeout: -1
+});
+
+export const createFiber = (element: React.ReactElement<any>): Fiber => {
+    const root = reconciler.createContainer({}, LegacyRoot, null, false, false, "", null, null);
+    reconciler.updateContainer(element, root);
+    return root.current;
+};
---
+++ dium/tests/utils/react.tsx
@@ -0,0 +1,109 @@
+import {describe, it} from "mocha";
+import {strict as assert} from "assert";
+import React from "react";
+import {createFiber} from "../mock";
+
+import {Direction, Predicate, queryFiber, queryTree, queryTreeAll} from "../../src/utils/react";
+import type {Fiber} from "../../src/react-internals";
+
+const TestComponent = ({children}: {children: JSX.Element}): JSX.Element => children;
+
+const elements = (
+    <div className="foo">
+        <TestComponent key={0}>
+            <span className="bar"/>
+        </TestComponent>
+        <span key={1} className="baz">
+            <text/>
+        </span>
+    </div>
+);
+
+describe("React element tree", () => {
+    const tree = elements;
+
+    describe("queryTree", () => {
+        it("finds a result", () => {
+            assert(queryTree(tree, (node) => node.type === "span") instanceof Object);
+        });
+
+        it("finds the correct node", () => {
+            assert.equal(queryTree(tree, (node) => node.type === "span"), tree.props.children[1]);
+        });
+    });
+
+    describe("queryTreeAll", () => {
+        it("finds a result", () => {
+            assert(queryTreeAll(tree, (node) => node.type === "span").length > 0);
+        });
+
+        it("finds the correct nodes", () => {
+            assert.deepEqual(
+                queryTreeAll(tree, (node) => node.type === "span"),
+                [tree.props.children[1], tree.props.children[0].props.children]
+            );
+        });
+    });
+});
+
+describe("React Fiber", () => {
+    const root = createFiber(elements);
+    const parent = root.child;
+    const child = parent.child.child;
+
+    const deepRoot = {key: "0"} as Fiber;
+    let deepChild = deepRoot;
+    for (let i = 1; i < 100; i++) {
+        const child = {
+            key: i.toString(),
+            return: deepChild
+        } as Fiber;
+        deepChild.child = child;
+        deepChild = child;
+    }
+
+    const createTrackingPredicate = (): [Predicate<Fiber>, Set<number>] => {
+        const calledOn = new Set<number>();
+        const predicate = (node: Fiber) => {
+            calledOn.add(parseInt(node.key));
+            return false;
+        };
+        return [predicate, calledOn];
+    };
+
+    describe("queryFiber", () => {
+        it("finds a result upwards", () => {
+            assert(queryFiber(child, (node) => node.type === "div", Direction.Up) instanceof Object);
+        });
+
+        it("finds the correct node upwards", () => {
+            assert.equal(queryFiber(parent, (node) => node.type === "div", Direction.Up), parent);
+        });
+
+        it("finds a result downwards", () => {
+            assert(queryFiber(parent, (node) => node.type === "span", Direction.Down) instanceof Object);
+        });
+
+        it("finds the correct node downwards", () => {
+            assert.equal(queryFiber(parent, (node) => node.type === "span", Direction.Down), child);
+        });
+
+        it("stops after max depth upwards", () => {
+            const depth = 30;
+            const [predicate, calledOn] = createTrackingPredicate();
+            queryFiber(deepChild, predicate, Direction.Up);
+
+            const expected = new Set([...Array(depth + 1).keys()].map((i) => 99 - i)); // includes call on node itself
+            assert.deepEqual(calledOn, expected);
+        });
+
+        it("stops after max depth downwards", () => {
+            const depth = 30;
+            const [predicate, calledOn] = createTrackingPredicate();
+            queryFiber(deepRoot, predicate, Direction.Down);
+
+            const expected = new Set(Array(depth + 1).keys()); // includes call on node itself
+            assert.deepEqual(calledOn, expected);
+        });
+    });
+});
