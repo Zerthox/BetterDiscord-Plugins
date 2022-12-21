021f457462c66f0dac9d5a00165ec2b7738e8bfa
Zerthox <zerthox@gmail.com>
2022-12-21 22:20:00 +0100

    Type text variants

--- dium/src/components/text.ts
+++ dium/src/components/text.ts
@@ -1,7 +1,7 @@
 import {Finder} from "../api";
 
 export interface TextProps {
-    variant?: string;
+    variant?: TextVariants;
     tag?: any;
     selectable?: boolean;
     className?: string;
@@ -9,5 +9,62 @@ export interface TextProps {
     color?: string;
     style?: React.CSSProperties;
 }
+
+type TextVariants =
+    "code"
+    | "display-lg"
+    | "display-md"
+    | "display-sm"
+    | "eyebrow"
+    | "heading-deprecated-12/bold"
+    | "heading-deprecated-12/extrabold"
+    | "heading-deprecated-12/medium"
+    | "heading-deprecated-12/normal"
+    | "heading-deprecated-12/semibold"
+    | "heading-lg/bold"
+    | "heading-lg/extrabold"
+    | "heading-lg/medium"
+    | "heading-lg/normal"
+    | "heading-lg/semibold"
+    | "heading-md/bold"
+    | "heading-md/extrabold"
+    | "heading-md/medium"
+    | "heading-md/normal"
+    | "heading-md/semibold"
+    | "heading-sm/bold"
+    | "heading-sm/extrabold"
+    | "heading-sm/medium"
+    | "heading-sm/normal"
+    | "heading-sm/semibold"
+    | "heading-xl/bold"
+    | "heading-xl/extrabold"
+    | "heading-xl/medium"
+    | "heading-xl/normal"
+    | "heading-xl/semibold"
+    | "heading-xxl/bold"
+    | "heading-xxl/extrabold"
+    | "heading-xxl/medium"
+    | "heading-xxl/normal"
+    | "heading-xxl/semibold"
+    | "text-lg/bold"
+    | "text-lg/medium"
+    | "text-lg/normal"
+    | "text-lg/semibold"
+    | "text-md/bold"
+    | "text-md/medium"
+    | "text-md/normal"
+    | "text-md/semibold"
+    | "text-sm/bold"
+    | "text-sm/medium"
+    | "text-sm/normal"
+    | "text-sm/semibold"
+    | "text-xs/bold"
+    | "text-xs/medium"
+    | "text-xs/normal"
+    | "text-xs/semibold"
+    | "text-xxs/bold"
+    | "text-xxs/medium"
+    | "text-xxs/normal"
+    | "text-xxs/semibold";
 
 export const Text: React.FunctionComponent<TextProps> = /* @__PURE__ */ Finder.bySource([".lineClamp", ".variant"], {entries: true});
