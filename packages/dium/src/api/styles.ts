e1175c925ce50f1987b29442663115c7a14dfebb
Zerthox <zerthox@gmail.com>
2023-01-27 17:44:47 +0100

    Inject styles into popout windows

--- packages/dium/src/api/styles.ts
+++ packages/dium/src/api/styles.ts
@@ -1,21 +1,47 @@
 import {getMeta} from "../meta";
+import {PopoutWindowStore} from "../modules/popout-window";
+
+const getOpenWindows = (): Window[] => PopoutWindowStore?.getWindowKeys()
+    .filter((key) => PopoutWindowStore.getWindowFocused(key))
+    .map((key) => PopoutWindowStore.getWindow(key)) ?? [];
+
+const popoutWindowListener = () => {
+    for (const popout of getOpenWindows()) {
+        const {name} = getMeta();
+        if (!popout.document.getElementById(name)) {
+            const el = popout.document.createElement("style");
+            el.id = name;
+            el.textContent = document.getElementById(name)?.textContent;
+            popout.document.head.append(el);
+        }
+    }
+};
 
 /** Inject CSS. */
 export const inject = (styles?: string): void => {
     if (typeof styles === "string") {
         BdApi.DOM.addStyle(getMeta().name, styles);
+        PopoutWindowStore?.removeChangeListener(popoutWindowListener);
+        PopoutWindowStore?.addChangeListener(popoutWindowListener);
     }
 };
 
 /** Clear previously injected CSS. */
-export const clear = (): void => BdApi.DOM.removeStyle(getMeta().name);
+export const clear = (): void => {
+    const {name} = getMeta();
+    BdApi.DOM.removeStyle(name);
+    PopoutWindowStore?.removeChangeListener(popoutWindowListener);
+    for (const popout of getOpenWindows()) {
+        popout.document.getElementById(name)?.remove();
+    }
+};
 
 /** Create suffixed versions for a list of class names. */
 export const suffix = <T extends string = never>(...classNames: T[]): Record<T, string> => {
     const result: Record<T, string> = {} as any;
     for (const className of classNames) {
         Object.defineProperty(result, className, {
-            get: () => {
+            get() {
                 const value = className + "-" + getMeta().name;
                 Object.defineProperty(result, className, {
                     value,
