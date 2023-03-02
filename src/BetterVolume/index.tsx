6c98fb1238d476e66a46d293acb340c8f0c8b66a
Zerthox <zerthox@gmail.com>
2023-03-02 18:01:31 +0100

    Disable remote audio settings experiment

--- src/BetterVolume/index.tsx
+++ src/BetterVolume/index.tsx
@@ -1,5 +1,5 @@
 import {createPlugin, Finder, Filters, Patcher, React} from "dium";
-import {Snowflake, MediaEngineStore, MediaEngineActions, MediaEngineContext} from "@dium/modules";
+import {Snowflake, MediaEngineStore, MediaEngineActions, MediaEngineContext, ExperimentStore} from "@dium/modules";
 import {MenuItem} from "@dium/components";
 import {NumberInput} from "./input";
 import {css} from "./styles.module.scss";
@@ -17,44 +17,48 @@ const AudioConvert: AudioConvert = Finder.demangle({
 type UseUserVolumeItem = (userId: Snowflake, context: MediaEngineContext) => JSX.Element;
 
 export default createPlugin({
-    async start() {
-        // wait for context menu lazy load
-        const filter = Filters.bySource("user-volume");
-        const useUserVolumeItem = Finder.resolveKey(
-            await Finder.waitFor(filter, {resolve: false}) as Record<string, UseUserVolumeItem>,
-            filter
-        );
+    start() {
+        // disable remote audio settings experiment
+        const audioExperiment = ExperimentStore.getUserExperimentDescriptor("2022-09_remote_audio_settings");
+        if (audioExperiment) {
+            // simply setting this should be fine, seems to be only changed on connect etc.
+            audioExperiment.bucket = 0;
+        }
 
-        // add number input
-        Patcher.after(...useUserVolumeItem, ({args: [userId, context], result}) => {
-            // check for original render
-            if (result) {
-                // we can read this directly, the original has a hook to ensure updates
-                const volume = MediaEngineStore.getLocalVolume(userId, context);
+        // add number input to user volume item
+        const useUserVolumeItemFilter = Filters.bySource("user-volume");
+        Finder.waitFor(useUserVolumeItemFilter, {resolve: false}).then((result: Record<string, UseUserVolumeItem>) => {
+            const useUserVolumeItem = Finder.resolveKey(result, useUserVolumeItemFilter);
+            Patcher.after(...useUserVolumeItem, ({args: [userId, context], result}) => {
+                // check for original render
+                if (result) {
+                    // we can read this directly, the original has a hook to ensure updates
+                    const volume = MediaEngineStore.getLocalVolume(userId, context);
 
-                return (
-                    <>
-                        {result}
-                        <MenuItem
-                            id="user-volume-input"
-                            render={() => (
-                                <NumberInput
-                                    value={AudioConvert.amplitudeToPerceptual(volume)}
-                                    min={0}
-                                    max={999999}
-                                    fallback={100}
-                                    onChange={(value) => MediaEngineActions.setLocalVolume(
-                                        userId,
-                                        AudioConvert.perceptualToAmplitude(value),
-                                        context
-                                    )}
-                                />
-                            )}
-                        />
-                    </>
-                );
-            }
-        }, {name: "useUserVolumeItem"});
+                    return (
+                        <>
+                            {result}
+                            <MenuItem
+                                id="user-volume-input"
+                                render={() => (
+                                    <NumberInput
+                                        value={AudioConvert.amplitudeToPerceptual(volume)}
+                                        min={0}
+                                        max={999999}
+                                        fallback={100}
+                                        onChange={(value) => MediaEngineActions.setLocalVolume(
+                                            userId,
+                                            AudioConvert.perceptualToAmplitude(value),
+                                            context
+                                        )}
+                                    />
+                                )}
+                            />
+                        </>
+                    );
+                }
+            }, {name: "useUserVolumeItem"});
+        });
     },
     styles: css
 });
