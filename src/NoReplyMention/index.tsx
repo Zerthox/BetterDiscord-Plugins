5ae080e7877a02688b015a812ad0f89334d1c834
Zerthox <zerthox@gmail.com>
2023-10-29 14:37:56 +0100

    Fix reply actions filters

--- src/NoReplyMention/index.tsx
+++ src/NoReplyMention/index.tsx
@@ -1,6 +1,9 @@
 import {createPlugin, Finder, Filters, Patcher} from "dium";
 
-const ReplyActions = Finder.demangle({createPendingReply: Filters.bySource(".shouldMention")}, null, true);
+const ReplyActions = Finder.demangle({
+    createPendingReply: Filters.bySource("shouldMention", "CREATE_PENDING_REPLY"),
+    deletePendingReply: Filters.bySource("DELETE_PENDING_REPLY")
+}, null, true);
 
 export default createPlugin({
     start() {
