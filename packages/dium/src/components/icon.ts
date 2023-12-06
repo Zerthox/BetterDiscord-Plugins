4ff7d37ded81b46f451503e22b8f339eeaa495d7
Zerthox <zerthox@gmail.com>
2023-12-06 14:01:17 +0100

    Update arrow icon filter (yet again)

--- packages/dium/src/components/icon.ts
+++ packages/dium/src/components/icon.ts
@@ -1,4 +1,4 @@
 import {Finder} from "../api";
 
 // chevron small down icon
-export const IconArrow = /* @__PURE__ */ Finder.bySource(["d:\"M5.29289 9"], {entries: true});
+export const IconArrow = /* @__PURE__ */ Finder.bySource(["d:\"M5.3 9."], {entries: true});
