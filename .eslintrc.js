0f5f99ceda79ad85e26192e2eec778af3950a6d5
Zerthox <zerthox@gmail.com>
2024-05-04 16:03:32 +0200

    Fix lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,14 +17,10 @@ module.exports = {
             version: "17.0.2" // stable Discord
         }
     },
-    parserOptions: {
-        project: ["./tsconfig.json"]
-    },
     extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        "google",
-        "plugin:@typescript-eslint/recommended"
+        "google"
     ],
     rules: {
         indent: "off",
@@ -44,19 +40,30 @@ module.exports = {
         "require-jsdoc": "off",
         "valid-jsdoc": "off",
         "react/display-name": "off",
-        "new-cap": "off",
-        "@typescript-eslint/indent": ["error", 4],
-        "@typescript-eslint/semi": "error",
-        "@typescript-eslint/quotes": ["error", "double"],
-        "@typescript-eslint/member-delimiter-style": ["error"],
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-            caughtErrorsIgnorePattern: "^_"
-        }],
-        "@typescript-eslint/prefer-optional-chain": "error",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-        "@typescript-eslint/no-explicit-any": "off"
-    }
+        "new-cap": "off"
+    },
+    overrides: [{
+        files: ["*.ts", "*.tsx"],
+        extends: [
+            "plugin:@typescript-eslint/recommended"
+        ],
+        parserOptions: {
+            project: ["./tsconfig.json"]
+        },
+        rules: {
+            "@typescript-eslint/indent": ["error", 4],
+            "@typescript-eslint/semi": "error",
+            "@typescript-eslint/quotes": ["error", "double"],
+            "@typescript-eslint/member-delimiter-style": ["error"],
+            "@typescript-eslint/no-unused-vars": ["error", {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_"
+            }],
+            "@typescript-eslint/prefer-optional-chain": "error",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "error",
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }]
 };
