affb01fe413e24939f7a201f85b32fd8ea991e15
Zerthox <zerthox@gmail.com>
2025-11-29 20:55:58 +0100

    Adjust style handling

--- packages/rollup-plugin-style-modules/index.ts
+++ packages/rollup-plugin-style-modules/index.ts
@@ -19,42 +19,57 @@ export interface Options {
 export function styleModules({ modules, cleanup = true }: Options = {}): Plugin {
     const filter = (id: string) => /\.module\.(css|scss|sass)$/.test(id);
 
+    const postcssCleanup: postcss.Plugin = {
+        postcssPlugin: "cleanup",
+        OnceExit(root) {
+            for (const child of root.nodes) {
+                if (child.type === "rule") {
+                    const contents = child.nodes.filter((node) => node.type !== "comment");
+                    if (contents.length === 0) {
+                        child.remove();
+                    }
+                }
+            }
+        },
+    };
+
     return {
         name: "style-modules",
         async transform(code, id) {
             if (filter(id)) {
-                // we expect stringified css string to be the default export
-                const [, cssString] = /\s*export\s+default\s+(.+)$/.exec(code) ?? [];
-                if (cssString) {
-                    const css = JSON.parse(cssString);
+                const program = this.parse(code);
+                const exported = program.body.find((node) => node.type === "ExportDefaultDeclaration")?.declaration;
+
+                let current = exported;
+                while (current?.type === "Identifier") {
+                    const name = current.name;
+                    for (const other of program.body) {
+                        if (other.type === "VariableDeclaration") {
+                            const decl = other.declarations.find(
+                                ({ id }) => id.type === "Identifier" && id.name === name,
+                            );
+                            if (decl) {
+                                current = decl.init;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (current?.type === "Literal" && typeof current.value === "string") {
+                    const css = current.value;
 
                     let mapping: Record<string, string>;
                     const result = await postcss()
                         .use(
                             postcssModules({
                                 ...modules,
-                                getJSON: (_file, json) => (mapping = json),
+                                getJSON: (_file, json) => {
+                                    mapping = json;
+                                },
                             }),
                         )
-                        .use(
-                            cleanup
-                                ? {
-                                      postcssPlugin: "cleanup",
-                                      OnceExit(root) {
-                                          for (const child of root.nodes) {
-                                              if (child.type === "rule") {
-                                                  const contents = child.nodes.filter(
-                                                      (node) => node.type !== "comment",
-                                                  );
-                                                  if (contents.length === 0) {
-                                                      child.remove();
-                                                  }
-                                              }
-                                          }
-                                      },
-                                  }
-                                : null,
-                        )
+                        .use(cleanup ? postcssCleanup : null)
                         .process(css, { from: id });
 
                     const named = Object.entries(mapping)
